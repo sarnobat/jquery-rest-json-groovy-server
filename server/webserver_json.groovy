77d84cf12e1f170f961907c62b80675a7732f508
Sridhar Sarnobat <ss401533@gmail.com>
2013-06-15 12:30:51 -0700

    Minor

--- server/webserver_json.groovy
+++ server/webserver_json.groovy
@@ -1,3 +1,6 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.json.JSONObject;
 import java.io.IOException;
@@ -9,13 +12,48 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 
+ 
 class MyHandler implements HttpHandler {
+	public Map<String, String> getQueryMap(String query)  
+	{  
+		println('getQueryMap() - 1');
+		Pattern pattern = Pattern.compile("/\\?.*");
+		
+		println('getQueryMap() - 1.1');
+		Matcher matcher = pattern.matcher(query);
+		println('getQueryMap() - 1.2 - ' + matcher.group());
+		String[] params = query.split("&");  
+		println('getQueryMap() - 2');
+		Map<String, String> map = new HashMap<String, String>();  
+		println('getQueryMap() - 3');
+		for (String param : params)  
+		{  
+			println('getQueryMap() - 4');
+			String name = param.split("=")[0];  
+			println('getQueryMap() - 5');
+			String value = param.split("=")[1];  
+			println('getQueryMap() - 6');
+			map.put(name, value);  
+			println('getQueryMap() - 7: ' + value);
+		}  
+		println('getQueryMap() - 8');
+		return map;  
+	}  
+
 	public void handle(HttpExchange t) throws IOException {
 		println('Request received');
 		JSONObject json = new JSONObject();
-		json.put("myKey","myValue");
+		print('1');
+		String query = t.getRequestURI();
+		print('2');
+		Map<String, String> map = getQueryMap(query);  
+		print('3');
+		String  value = map.get("param1");
+		print('4');
+		json.put("myKey",value);
 		println('Request headers: ' + t.getRequestHeaders());
 		println('Request URI' + t.getRequestURI());
+		println('value: ' + value);
 		t.getResponseHeaders().add("Access-Control-Allow-Origin","*");
 		t.sendResponseHeaders(200, json.toString().length());
 		OutputStream os = t.getResponseBody();
